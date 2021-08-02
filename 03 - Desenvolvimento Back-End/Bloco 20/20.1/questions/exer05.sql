1f4a54fe2eb0c43fd64043fda837e7deb8575228
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-02 20:17:03 -0300

    20.1 exer05 quest05

--- 03 - Desenvolvimento Back-End/Bloco 20/20.1/questions/exer05.sql
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.1/questions/exer05.sql
@@ -15,7 +15,8 @@
 -- Exercício 4: Analise a tabela city e encontre a tabela à qual a coluna country_id faz referência.
 -- R: Faz referência a tabela "country".
 
--- Exercício 5: Após resolver o exercício anterior, responda: qual tipo de relacionamento a tabela city faz com a tabela country ?
+-- Exercício 5: Após resolver o exercício anterior, responda: qual tipo de relacionamento a tabela city faz com a tabela country?
+-- R: Relacionamento 1:N.
 
 -- Exercício 6: Qual tipo de relacionamento a tabela country faz com a tabela city ?
 
