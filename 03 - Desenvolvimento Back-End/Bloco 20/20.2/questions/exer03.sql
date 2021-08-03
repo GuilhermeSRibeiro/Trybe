f5bfa458d12129ac4b4078ecbe19bca89e31a577
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-03 03:05:10 -0300

    20.2 exer03 quest01

--- 03 - Desenvolvimento Back-End/Bloco 20/20.2/questions/exer03.sql
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.2/questions/exer03.sql
@@ -1,6 +1,6 @@
 USE sakila;
 -- 1. Na tabela sakila.film, monte uma query que exiba o título e o ano de lançamento dos filmes em uma coluna e dê a ela o nome Lançamento do Filme.
-
+SELECT CONCAT(title, ' ', release_year) AS 'Lançamento do Filme' FROM film;
 
 -- 2. Na tabela sakila.film, crie uma query que exiba o título do filme e sua classificação indicativa (PG, G, NC-17) em apenas uma coluna. Dê a ela o nome Classificação. Não se esqueça de deixar um espaço entre as palavras para que fiquem legíveis.
 
