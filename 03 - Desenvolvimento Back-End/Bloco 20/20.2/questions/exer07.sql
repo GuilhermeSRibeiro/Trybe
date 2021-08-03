d9d90e9a1c3c7752bba43294fc19484408e81665
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-03 04:00:14 -0300

    20.2 cria exer07

---
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.2/questions/exer07.sql
@@ -0,0 +1,19 @@
+-- Para os exercícios a seguir, vamos usar a tabela sakila.actor
+USE sakila;
+-- 1. Escreva uma query que exiba apenas os sobrenomes únicos cadastrados.
+
+
+-- 2. Quantos sobrenomes únicos temos na tabela?
+
+
+-- 3. Ordene os valores na tabela em ordem crescente de sobrenomes e em ordem decrescente de nome.
+
+
+-- 4. Vá até a tabela language do sakila e crie uma pesquisa que mostre os 5 idiomas cadastrados, mas não mostre o idioma english.
+
+
+-- 5. Vá até a tabela film e selecione todos os dados da tabela. Pronto, fez isso?
+
+
+-- 6. Agora vamos tentar fazer o seguinte: Crie uma query para encontrar os 20 primeiros filmes, incluindo o título, o ano de lançamento, a duração, a classificação indicativa e o custo de substituição. Ordene os resultados pelos filmes com a maior duração e depois pelo menor custo de substituição.
+
