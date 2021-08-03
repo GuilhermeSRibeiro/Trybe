1173d0dd291965a1699fe1d9c708ef4d055f39e1
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-03 03:52:52 -0300

    20.2 exer06 quest02

--- 03 - Desenvolvimento Back-End/Bloco 20/20.2/questions/exer06.sql
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.2/questions/exer06.sql
@@ -4,7 +4,7 @@ USE sakila;
 SELECT * FROM film;
 
 -- 2. Escreva uma query que exiba apenas o nome dos filmes, seu ano de lançamento e sua classificação.
-
+SELECT title, release_year, rating FROM film;
 
 -- 3. Quantos filmes temos cadastrados?
 
