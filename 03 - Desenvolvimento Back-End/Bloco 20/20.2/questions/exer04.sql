9382b831b08e26f72d8226a4f07a76410bf5dbc0
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-03 03:20:17 -0300

    20.2 cria exer04.sql

---
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.2/questions/exer04.sql
@@ -0,0 +1,18 @@
+USE Escola;
+-- 1. Monte uma query para encontrar pares únicos de nomes e idades.
+
+
+-- 2. Quantas linhas você encontraria na query anterior?
+
+
+-- 3. Monte uma query para encontrar somente os nomes únicos.
+
+
+-- 4. Quantas linhas você encontraria na query anterior?
+
+
+-- 5. Monte uma query para encontrar somente as idades únicas.
+
+
+-- 6. Quantas linhas você encontraria na query anterior?
+
