b5df5753d21532e94ee7cfcf3f222cd1a26230e0
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-03 03:07:16 -0300

    20.2 exer03 quest02

--- 03 - Desenvolvimento Back-End/Bloco 20/20.2/questions/exer03.sql
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.2/questions/exer03.sql
@@ -3,7 +3,7 @@ USE sakila;
 SELECT CONCAT(title, ' ', release_year) AS 'Lançamento do Filme' FROM film;
 
 -- 2. Na tabela sakila.film, crie uma query que exiba o título do filme e sua classificação indicativa (PG, G, NC-17) em apenas uma coluna. Dê a ela o nome Classificação. Não se esqueça de deixar um espaço entre as palavras para que fiquem legíveis.
-
+SELECT CONCAT(title, ' ', rating) AS 'Classificação' FROM film;
 
 -- 3. Na tabela sakila.address, monte uma query que exiba a rua e o distrito de cada registro em uma coluna apenas, e dê a essa coluna o nome Endereço.
 
