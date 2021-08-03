b2774cc23e6ef839582955fec50faf1562ed455f
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-03 03:26:17 -0300

    20.2 exer04 quest01

--- 03 - Desenvolvimento Back-End/Bloco 20/20.2/questions/exer04.sql
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.2/questions/exer04.sql
@@ -1,6 +1,6 @@
 USE Escola;
 -- 1. Monte uma query para encontrar pares únicos de nomes e idades.
-
+SELECT DISTINCT Nome, Idade FROM Alunos;
 
 -- 2. Quantas linhas você encontraria na query anterior?
 
