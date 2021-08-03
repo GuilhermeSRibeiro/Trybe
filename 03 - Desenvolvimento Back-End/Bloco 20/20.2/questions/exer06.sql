456b7e4af1d99555e50aefe40264c9735ec91c2d
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-03 03:49:12 -0300

    20.2 cria exer06.sql

---
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.2/questions/exer06.sql
@@ -0,0 +1,10 @@
+-- Para os exercícios a seguir, vamos usar a tabela sakila.film
+USE sakila;
+-- 1. Escreva uma query que exiba todos os filmes cadastrados no banco de dados.
+
+
+-- 2. Escreva uma query que exiba apenas o nome dos filmes, seu ano de lançamento e sua classificação.
+
+
+-- 3. Quantos filmes temos cadastrados?
+
