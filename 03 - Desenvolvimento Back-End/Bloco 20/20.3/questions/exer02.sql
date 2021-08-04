79df53311bac5e1f4f9b23aeeeded6008d03523c
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-04 14:11:44 -0300

    20.3 exer02 quest05

--- 03 - Desenvolvimento Back-End/Bloco 20/20.3/questions/exer02.sql
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.3/questions/exer02.sql
@@ -17,8 +17,10 @@ AND title LIKE '%lord';
 SELECT * FROM film
 WHERE title LIKE '___gon%';
 
--- 5. Mostre o único caso em que, a partir do 4° caractere no título do filme, tem-se a palavra gon e a descrição contém a palavra Documentary .
-
+-- 5. Mostre o único caso em que, a partir do 4° caractere no título do filme, tem-se a palavra gon e a descrição contém a palavra Documentary.
+SELECT * FROM film
+WHERE title LIKE '___gon%'
+AND description LIKE '%Documentary%'
 
 -- 6. Mostre os dois filmes cujos títulos ou finalizam com academy ou iniciam com mosquito .
 
