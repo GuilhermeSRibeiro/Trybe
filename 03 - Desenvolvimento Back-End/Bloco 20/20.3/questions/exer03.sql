ed53cbbc706f24c65e712a4a2bfa9b4cdc0f88ec
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-04 14:51:58 -0300

    20.3 exer03 quest05

--- 03 - Desenvolvimento Back-End/Bloco 20/20.3/questions/exer03.sql
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.3/questions/exer03.sql
@@ -20,4 +20,7 @@ WHERE rental_duration BETWEEN (3) AND (6)
 ORDER BY rental_duration DESC, title;
 
 -- 5. Monte um relatório que exiba o título e classificação dos 500 primeiros filmes direcionados para as classificações indicativas G, PG e PG-13. Os resultados devem estar ordenados por título.
-
+SELECT title, rating FROM film
+WHERE rating IN ('G','PG','PG-13')
+ORDER BY title
+LIMIT 500;
