b055a7f44fa1ef5c14272ac0d17893cdbefcab29
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-04 07:54:37 -0300

    20.2 exer08 quest01

--- 03 - Desenvolvimento Back-End/Bloco 20/20.2/questions/exer08.sql
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.2/questions/exer08.sql
@@ -1,7 +1,7 @@
 USE Scientists;
 
 -- 1. Escreva uma query para exibir a string "This is SQL Exercise, Practice and Solution".
-
+SELECT 'This is SQL Exercise, Practice and Solution' AS 'Answer';
 
 -- 2. Escreva uma query para exibir três números em três colunas.
 
