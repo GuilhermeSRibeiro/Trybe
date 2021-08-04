f8081a7d5ce911615741c454ca151e878c54f929
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-04 16:28:50 -0300

    20.3 exer04 quest03

--- 03 - Desenvolvimento Back-End/Bloco 20/20.3/questions/exer04.sql
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.3/questions/exer04.sql
@@ -9,7 +9,15 @@ SELECT COUNT(*) AS 'Pagamentos feitos entre 01/07/2005 e 22/08/2005' FROM paymen
 WHERE payment_date BETWEEN ('2005-07-01') AND ('2005-08-22');
 
 -- 3. Usando a tabela rental, extraia data, ano, mês, dia, hora, minuto e segundo dos registros com rental_id = 10330. Utilize a coluna rental_date para extrair as informações.
-
+SELECT DATE(rental_date) AS 'Data',
+YEAR(rental_date) AS 'Ano',
+MONTH(rental_date) AS 'Mês',
+DAY(rental_date) AS 'Dia',
+HOUR(rental_date) AS 'Horas',
+MINUTE(rental_date) AS 'Minutos',
+SECOND(rental_date) AS 'Segundos'
+FROM rental
+WHERE rental_id = 10330;
 
 -- 4. Monte uma query que retorne todos os dados do pagamento feito no dia 28/07/2005 a partir das 22 horas.
 
