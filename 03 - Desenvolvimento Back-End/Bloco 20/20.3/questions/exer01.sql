4c94ed50d83ebbe4bf6d4abf161a8d32e1c1d24d
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-04 13:42:16 -0300

    20.3 exer01 quest03

--- 03 - Desenvolvimento Back-End/Bloco 20/20.3/questions/exer01.sql
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.3/questions/exer01.sql
@@ -12,7 +12,11 @@ AND first_name <> 'KENNETH'
 ORDER BY first_name, last_name;
 
 -- 3. O setor financeiro quer saber título, descrição, ano de lançamento e valor do custo de substituição (replacement_cost), dos 100 filmes com o maior custo de substituição, do valor mais alto ao mais baixo, entre os filmes feitos para menores de idade e que têm o custo mínimo de substituição de $18,00 dólares. Em caso de empate, ordene em ordem alfabética pelo título.
-
+SELECT title, description, release_year, replacement_cost FROM film
+WHERE rating <> 'NC-17'
+AND replacement_cost > 18
+ORDER BY replacement_cost DESC, title
+LIMIT 100;
 
 -- 4. Quantos clientes estão ativos e na loja 1?
 
