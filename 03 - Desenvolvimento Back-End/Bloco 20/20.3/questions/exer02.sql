92779d254afbd37fd6423c04c72d78e4f4749594
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-04 14:16:09 -0300

    20.3 exer02 quest07

--- 03 - Desenvolvimento Back-End/Bloco 20/20.3/questions/exer02.sql
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.3/questions/exer02.sql
@@ -28,4 +28,6 @@ WHERE title LIKE '%academy'
 OR title LIKE 'mosquito%';
 
 -- 7. Mostre os seis filmes que contêm as palavras monkey e sumo em suas descrições.
-
+SELECT * FROM film
+WHERE description LIKE '%monkey%'
+AND description LIKE '%sumo%';
