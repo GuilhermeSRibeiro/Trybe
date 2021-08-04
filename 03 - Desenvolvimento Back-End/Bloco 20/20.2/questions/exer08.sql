bbc0823e3216b4cf64f001f82a339ef2025f2d11
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-04 09:30:05 -0300

    20.2 exer08 quest15

--- 03 - Desenvolvimento Back-End/Bloco 20/20.2/questions/exer08.sql
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.2/questions/exer08.sql
@@ -54,4 +54,4 @@ ORDER BY Hours
 LIMIT 5;
 
 -- 15. Escreva uma query que exiba a string "Existem Number cientistas na tabela Scientists.", em que Number se refira a quantidade de cientistas.
-
+SELECT CONCAT('Existem ', COUNT(*), ' cientistas na tabela Scientists.') AS 'Quantos cientistas existem na tabela Scientists?' FROM Scientists;
