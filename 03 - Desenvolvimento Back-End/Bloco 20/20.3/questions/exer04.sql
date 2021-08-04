a2e06e98d741de444b4479577fd0f3acf941aa47
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-04 16:32:10 -0300

    20.3 exer04 quest04

--- 03 - Desenvolvimento Back-End/Bloco 20/20.3/questions/exer04.sql
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.3/questions/exer04.sql
@@ -20,4 +20,6 @@ FROM rental
 WHERE rental_id = 10330;
 
 -- 4. Monte uma query que retorne todos os dados do pagamento feito no dia 28/07/2005 a partir das 22 horas.
-
+SELECT * FROM payment
+WHERE DATE(payment_date) = '2005-07-28'
+AND HOUR(payment_date) >= 22;
