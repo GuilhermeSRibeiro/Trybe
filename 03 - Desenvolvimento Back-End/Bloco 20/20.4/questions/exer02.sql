7f006dbbf38f681887b94dc1e99c5ec3c2f7a859
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-04 22:37:43 -0300

    20.4 exer02 quest03

--- 03 - Desenvolvimento Back-End/Bloco 20/20.4/questions/exer02.sql
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.4/questions/exer02.sql
@@ -12,7 +12,11 @@ SET name = 'Science Fiction'
 WHERE name = 'Sci-Fi';
 
 -- 3. Atualize o valor do aluguel para $25 de todos os filmes com duração maior que 100 minutos e que possuem a classificações "G", "PG" ou "PG-13" e um custo de substituição maior que $20.
-
+UPDATE film
+SET rental_rate = 25
+WHERE length > 100
+AND rating IN ('G','PG','PG-13')
+AND replacement_cost > 20;
 
 -- 4. Foi determinado pelo setor financeiro que haverá um reajuste em todos os preços dos filmes, com base em sua duração. Para todos os filmes com duração entre 0 e 100, o valor do aluguel passará a ser $10,00, e o aluguel dos filmes com duração acima de 100 passará a ser de $20,00.
 
