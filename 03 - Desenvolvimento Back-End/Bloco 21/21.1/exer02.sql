9a8ce0f7b6a5393c3be5d8253e900d48c3cf8483
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-05 10:48:40 -0300

    21.1 exer02 quest02

--- 03 - Desenvolvimento Back-End/Bloco 21/21.1/exer02.sql
+++ 03 - Desenvolvimento Back-End/Bloco 21/21.1/exer02.sql
@@ -10,3 +10,12 @@ FROM film;
 	-- PG-13: "Não recomendado para menores de 13 anos";
 	-- R: "Não recomendado para menores de 17 anos";
 	-- Se não cair em nenhuma das classificações anteriores: "Proibido para menores de idade".
+SELECT title, rating,
+CASE
+	WHEN rating = 'G' THEN 'Livre para todos'
+    WHEN rating = 'PG' THEN 'Não recomendado para menores de 10 anos'
+    WHEN rating = 'PG-13' THEN 'Não recomendado para menores de 13 anos'
+    WHEN rating = 'R' THEN 'Não recomendado para menores de 17 anos'
+    ELSE 'Proibido para menores de idade'
+END AS 'público-alvo'
+FROM film;
