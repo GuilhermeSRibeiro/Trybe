217d2d48b9a6f6e2cc7af6d6f00d7557df3342ed
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-04 22:31:14 -0300

    20.4 exer02 quest01

--- 03 - Desenvolvimento Back-End/Bloco 20/20.4/questions/exer02.sql
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.4/questions/exer02.sql
@@ -1,7 +1,10 @@
 USE sakila;
+SET SQL_SAFE_UPDATES = 0;
 
 -- 1. Atualize o primeiro nome de todas as pessoas da tabela sakila.actor que possuem o primeiro nome "JULIA" para "JULES".
-
+UPDATE actor
+SET first_name = 'JULES'
+WHERE first_name = 'JULIA';
 
 -- 2. Foi exigido que a categoria "Sci-Fi" seja alterada para "Science Fiction".
 
