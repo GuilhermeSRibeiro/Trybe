a4af1f2170f272f76156574c5f61188115f0500b
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-05 11:17:53 -0300

    21.1 exer05 quest01

--- 03 - Desenvolvimento Back-End/Bloco 21/21.1/exer05.sql
+++ 03 - Desenvolvimento Back-End/Bloco 21/21.1/exer05.sql
@@ -1,5 +1,5 @@
 -- 1. Monte uma query que exiba a diferença de dias entre '2030-01-20' e hoje.
-
+SELECT DATEDIFF('2030-01-20',CURRENT_DATE()) AS 'Diferença entre 20/01/2030 e hoje';
 
 -- 2. Monte uma query exiba a diferença de horas entre '10:25:45' e '11:00:00'.
 
