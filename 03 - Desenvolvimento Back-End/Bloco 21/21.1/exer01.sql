f7550c7d3a6cbc3887e70408eafced38a9d349ba
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-05 07:38:39 -0300

    21.1 exer01 quest02

--- 03 - Desenvolvimento Back-End/Bloco 21/21.1/exer01.sql
+++ 03 - Desenvolvimento Back-End/Bloco 21/21.1/exer01.sql
@@ -1,8 +1,8 @@
 -- 1. Faça uma query que exiba a palavra 'trybe' em CAIXA ALTA.
 SELECT UCASE('Trybe') AS 'Caixa alta';
 
--- 2. Faça uma query que transforme a frase 'Você já ouviu falar do DuckDuckGo?' em 'Você já ouviu falar do Google?' .
-
+-- 2. Faça uma query que transforme a frase 'Você já ouviu falar do DuckDuckGo?' em 'Você já ouviu falar do Google?'.
+SELECT REPLACE('Você já ouviu falar do DuckDuckGo?','DuckDuckGo','Google') AS 'Substitui ocorrências';
 
 -- 3. Utilizando uma query , encontre quantos caracteres temos em 'Uma frase qualquer' .
 
