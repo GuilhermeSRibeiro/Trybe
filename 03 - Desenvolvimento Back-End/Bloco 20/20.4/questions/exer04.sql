4fd88eec3f386a71f1128a84a4c77a0aa6697d2f
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-04 23:41:20 -0300

    20.4 exer04 quest09

--- 03 - Desenvolvimento Back-End/Bloco 20/20.4/questions/exer04.sql
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.4/questions/exer04.sql
@@ -60,7 +60,9 @@ SET rating = 9.0
 WHERE domestic_sales > 400000000;
 
 -- Exercício 9: Altere a classificação da tabela BoxOffice para 6.0 de todos os filmes que lucraram menos de 300 milhões no mercado internacional e mais de 200 milhões no mercado interno.
-
+UPDATE BoxOffice
+SET rating = 6.0
+WHERE domestic_sales > 200000000 AND international_sales < 300000000;
 
 -- Exercício 10: Exclua da tabela Movies todos os filmes com menos de 100 minutos de duração.
 
