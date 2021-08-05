c644a41e79f925f21d68c1290af9504666ba5974
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-04 21:41:45 -0300

    20.4 exer01 quest03

--- 03 - Desenvolvimento Back-End/Bloco 20/20.4/questions/exer01.sql
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.4/questions/exer01.sql
@@ -10,7 +10,10 @@ INSERT INTO staff (first_name, last_name, address_id, store_id, active, username
 ('Maria','Ribeiro',1,1,1,'Maria');
 
 -- 3. Feito o exercício anterior, vamos agora para o nível 2. Insira dois funcionários novos em apenas uma query.
-
+INSERT INTO actor (first_name, last_name)
+SELECT first_name, last_name FROM customer
+ORDER BY customer_id
+LIMIT 5;
 
 -- 4. Selecione os cinco primeiros nomes e sobrenomes da tabela sakila.customer e cadastre essas pessoas como atores na tabela sakila.actor.
 
