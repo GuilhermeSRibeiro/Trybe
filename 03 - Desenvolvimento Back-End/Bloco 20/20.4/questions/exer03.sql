52674d1b7976506db67422ce61dbad03424701c6
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-04 23:02:44 -0300

    20.4 exer03 quest02

--- 03 - Desenvolvimento Back-End/Bloco 20/20.4/questions/exer03.sql
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.4/questions/exer03.sql
@@ -11,7 +11,14 @@ DELETE FROM actor
 WHERE first_name = 'KARL';
 
 -- 2. Exclua do banco de dados os atores com o nome de "MATTHEW".
+SELECT actor_id FROM actor
+WHERE first_name = 'MATTHEW';
 
+DELETE FROM film_actor
+WHERE actor_id IN (8,103,181);
+
+DELETE FROM actor
+WHERE first_name = 'MATTHEW';
 
 -- 3. Exclua da tabela film_text todos os registros que possuem a palavra "saga" em suas descrições.
 
