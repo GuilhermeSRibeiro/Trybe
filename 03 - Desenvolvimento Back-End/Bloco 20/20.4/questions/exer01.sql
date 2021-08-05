f593937442f3d00411d283e075f80703122a8fdd
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-04 21:37:04 -0300

    20.4 exer01 quest02

--- 03 - Desenvolvimento Back-End/Bloco 20/20.4/questions/exer01.sql
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.4/questions/exer01.sql
@@ -5,7 +5,9 @@ INSERT INTO staff (first_name, last_name, address_id, store_id, active, username
 VALUES ('Guilherme','Ribeiro',1,1,1,'Guilherme');
 
 -- 2. Para saber quais campos são obrigatórios, clique com o botão direito na tabela sakila.staff e selecione "Table Inspector". Clique na aba "columns" e verifique quais campos aceitam nulos para te guiar. Lembre-se de que valores que são gerados automaticamente não precisam ser inseridos manualmente. Boa explorada!
-
+INSERT INTO staff (first_name, last_name, address_id, store_id, active, username) VALUES
+('Getúlio','Santos',1,1,1,'Getúlio'),
+('Maria','Ribeiro',1,1,1,'Maria');
 
 -- 3. Feito o exercício anterior, vamos agora para o nível 2. Insira dois funcionários novos em apenas uma query.
 
