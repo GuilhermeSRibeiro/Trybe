89dea9b063f441ba94579b74c1443447f9abcfbc
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-04 23:41:07 -0300

    20.4 exer04 quest08

--- 03 - Desenvolvimento Back-End/Bloco 20/20.4/questions/exer04.sql
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.4/questions/exer04.sql
@@ -53,3 +53,14 @@ WHERE movie_id IN (2, 9);
  
 DELETE FROM Movies
 WHERE director = 'Andrew Staton';
+
+-- Exercício 8: Altere a classificação da tabela BoxOffice para 9.0 de todos os filmes que lucraram mais de 400 milhões no mercado interno.
+UPDATE BoxOffice
+SET rating = 9.0
+WHERE domestic_sales > 400000000;
+
+-- Exercício 9: Altere a classificação da tabela BoxOffice para 6.0 de todos os filmes que lucraram menos de 300 milhões no mercado internacional e mais de 200 milhões no mercado interno.
+
+
+-- Exercício 10: Exclua da tabela Movies todos os filmes com menos de 100 minutos de duração.
+
