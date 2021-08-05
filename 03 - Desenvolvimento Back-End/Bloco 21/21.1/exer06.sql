e2414384af5051334196716ad7051620695a7d4c
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-05 11:25:15 -0300

    21.1 exer06 quest01

--- 03 - Desenvolvimento Back-End/Bloco 21/21.1/exer06.sql
+++ 03 - Desenvolvimento Back-End/Bloco 21/21.1/exer06.sql
@@ -1,6 +1,16 @@
--- 1. Monte um query que exiba:
-	-- A média de duração dos filmes e dê o nome da coluna de 'Média de Duração';
-	-- A duração mínima dos filmes como 'Duração Mínima';
-	-- A duração máxima dos filmes como 'Duração Máxima';
-	-- A soma de todas as durações como 'Tempo de Exibição Total';
-	-- E finalmente, a quantidade total de filmes cadastrados na tabela sakila.film como 'Filmes Registrados'.
+USE sakila;
+-- Monte um query que exiba:
+-- 1. A média de duração dos filmes e dê o nome da coluna de 'Média de Duração';
+SELECT AVG(length) AS 'Média de Duração' FROM film;
+
+-- 2. A duração mínima dos filmes como 'Duração Mínima';
+
+
+-- 3. A duração máxima dos filmes como 'Duração Máxima';
+
+
+-- 4. A soma de todas as durações como 'Tempo de Exibição Total';
+
+
+-- 5. E finalmente, a quantidade total de filmes cadastrados na tabela sakila.film como 'Filmes Registrados'.
+
