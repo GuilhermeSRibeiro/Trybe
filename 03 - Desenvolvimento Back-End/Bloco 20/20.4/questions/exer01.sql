63b8da01e30dbc7418c15be6082b700654c19666
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-04 21:44:51 -0300

    20.4 exer01 corrigindo numeraçao

--- 03 - Desenvolvimento Back-End/Bloco 20/20.4/questions/exer01.sql
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.4/questions/exer01.sql
@@ -1,25 +1,23 @@
 USE sakila;
 
 -- 1. Insira um novo funcionário na tabela sakila.staff.
+-- Para saber quais campos são obrigatórios, clique com o botão direito na tabela sakila.staff e selecione "Table Inspector". Clique na aba "columns" e verifique quais campos aceitam nulos para te guiar. Lembre-se de que valores que são gerados automaticamente não precisam ser inseridos manualmente. Boa explorada!
 INSERT INTO staff (first_name, last_name, address_id, store_id, active, username)
 VALUES ('Guilherme','Ribeiro',1,1,1,'Guilherme');
 
--- 2. Para saber quais campos são obrigatórios, clique com o botão direito na tabela sakila.staff e selecione "Table Inspector". Clique na aba "columns" e verifique quais campos aceitam nulos para te guiar. Lembre-se de que valores que são gerados automaticamente não precisam ser inseridos manualmente. Boa explorada!
+-- 2. Feito o exercício anterior, vamos agora para o nível 2. Insira dois funcionários novos em apenas uma query.
 INSERT INTO staff (first_name, last_name, address_id, store_id, active, username) VALUES
 ('Getúlio','Santos',1,1,1,'Getúlio'),
 ('Maria','Ribeiro',1,1,1,'Maria');
 
--- 3. Feito o exercício anterior, vamos agora para o nível 2. Insira dois funcionários novos em apenas uma query.
+-- 3. Selecione os cinco primeiros nomes e sobrenomes da tabela sakila.customer e cadastre essas pessoas como atores na tabela sakila.actor.
 INSERT INTO actor (first_name, last_name)
 SELECT first_name, last_name FROM customer
 ORDER BY customer_id
 LIMIT 5;
 
--- 4. Selecione os cinco primeiros nomes e sobrenomes da tabela sakila.customer e cadastre essas pessoas como atores na tabela sakila.actor.
+-- 4. Cadastre três categorias de uma vez só na tabela sakila.category.
 
 
--- 5. Cadastre três categorias de uma vez só na tabela sakila.category.
-
-
--- 6. Cadastre uma nova loja na tabela sakila.store.
+-- 5. Cadastre uma nova loja na tabela sakila.store.
 
