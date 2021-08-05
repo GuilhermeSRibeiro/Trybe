55bac5ce886648cdc20096de8bc92ee17dbbae7a
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-04 21:30:05 -0300

    20.4 exer01 quest01

--- 03 - Desenvolvimento Back-End/Bloco 20/20.4/questions/exer01.sql
+++ 03 - Desenvolvimento Back-End/Bloco 20/20.4/questions/exer01.sql
@@ -1,7 +1,8 @@
 USE sakila;
 
 -- 1. Insira um novo funcionário na tabela sakila.staff.
-
+INSERT INTO staff (first_name, last_name, address_id, store_id, active, username)
+VALUES ('Guilherme','Ribeiro',1,1,1,'Guilherme');
 
 -- 2. Para saber quais campos são obrigatórios, clique com o botão direito na tabela sakila.staff e selecione "Table Inspector". Clique na aba "columns" e verifique quais campos aceitam nulos para te guiar. Lembre-se de que valores que são gerados automaticamente não precisam ser inseridos manualmente. Boa explorada!
 
