cfdceaed70e0a22b6ea4a348f64df72342666288
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-05 21:20:56 -0300

    21.1 exer07 quest03

--- 03 - Desenvolvimento Back-End/Bloco 21/21.1/exer07.sql
+++ 03 - Desenvolvimento Back-End/Bloco 21/21.1/exer07.sql
@@ -10,7 +10,10 @@ FROM customer
 GROUP BY store_id, active;
 
 -- 3. Monte uma query que exiba a média de duração de locação por classificação indicativa ( rating ) dos filmes cadastrados na tabela sakila.film . Os resultados devem ser agrupados pela classificação indicativa e ordenados da maior média para a menor.
-
+SELECT AVG(rental_duration) AS `Média`, rating
+FROM film
+GROUP BY rating
+ORDER BY `Média` DESC;
 
 -- 4. Monte uma query para a tabela sakila.address que exiba o nome do distrito e a quantidade de endereços registrados nele. Os resultados devem ser ordenados da maior quantidade para a menor.
 
