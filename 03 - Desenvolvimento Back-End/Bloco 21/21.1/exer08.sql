b7d25d33c12bd124a394a974a68b7f8fe09153dd
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-05 21:34:43 -0300

    21.1 exer08 quest02

--- 03 - Desenvolvimento Back-End/Bloco 21/21.1/exer08.sql
+++ 03 - Desenvolvimento Back-End/Bloco 21/21.1/exer08.sql
@@ -7,6 +7,8 @@ HAVING `Média de duração` BETWEEN (115) AND (121.5)
 ORDER BY `Média de duração` DESC;
 
 -- 2. Usando a query a seguir, exiba apenas os valores de custo de substituição que estão acima de $3950.50. Dê um alias que faça sentido para SUM(replacement_cost) , de forma que deixe a query mais legível. Finalize ordenando os resultados de forma crescente.
-SELECT rating, SUM(replacement_cost)
-FROM sakila.film
-GROUP by rating;
+SELECT rating, SUM(replacement_cost) AS `Custo de substituição`
+FROM film
+GROUP by rating
+HAVING `Custo de substituição` > 3950.50
+ORDER BY `Custo de substituição`;
