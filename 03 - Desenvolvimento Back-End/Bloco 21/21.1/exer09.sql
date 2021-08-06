7acb8ddc8a64d016d34fa26d91ac7e0cb5da3aa5
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-05 23:54:31 -0300

    21.1 exer09 quest04

--- 03 - Desenvolvimento Back-End/Bloco 21/21.1/exer09.sql
+++ 03 - Desenvolvimento Back-End/Bloco 21/21.1/exer09.sql
@@ -14,7 +14,7 @@ GROUP BY JOB_ID
 ORDER BY `Média salarial` DESC;
 
 -- 4. Escreva uma query que exiba a quantidade de dinheiro necessária para realizar o pagamento de todas as pessoas funcionárias.
-
+SELECT SUM(SALARY) AS 'Total de salários' FROM employees;
 
 -- 5. Escreva uma query que exiba quatro informações: o maior salário, o menor salário, a soma de todos os salários e a média dos salários. Todos os valores devem ser formatados para ter apenas duas casas decimais.
 
