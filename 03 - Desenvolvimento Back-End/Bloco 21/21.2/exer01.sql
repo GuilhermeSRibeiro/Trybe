810ab5da521f2b9cf0074e0ece65c788b003936f
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-06 13:02:51 -0300

    21.2 exer01 quest01

--- 03 - Desenvolvimento Back-End/Bloco 21/21.2/exer01.sql
+++ 03 - Desenvolvimento Back-End/Bloco 21/21.2/exer01.sql
@@ -1,5 +1,10 @@
 USE sakila;
 -- 1. Monte uma query que exiba o id do ator, nome do ator e id do filme em que ele já atuou usando as tabelas actor e film_actor.
+SELECT actor.actor_id AS 'ID ator', CONCAT(actor.first_name,' ',actor.last_name) AS 'Noma completo', fa.film_id AS 'ID filme'
+FROM actor
+INNER JOIN film_actor fa
+ON actor.actor_id = fa.actor_id;
+
 -- 2. Use o JOIN para exibir o nome, sobrenome e endereço de cada um dos funcionários do banco. Use as tabelas staff e address.
 -- 3. Exiba o id do cliente, nome e email dos primeiros 100 clientes, ordenados pelo nome em ordem decrescente, juntamente com o id do endereço e o nome da rua onde o cliente mora. Essas informações podem ser encontradas nas tabelas customer e address.
 -- 4. Exiba o nome, email, id do endereço, endereço e distrito dos clientes que moram no distrito da California e que contêm "rene" em seus nomes. As informações podem ser encontradas nas tabelas address e customer.
