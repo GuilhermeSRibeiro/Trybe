298317e8f465b493ce1ea40f0846c39b1ef511de
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-05 23:51:01 -0300

    21.1 exer09 quest02

--- 03 - Desenvolvimento Back-End/Bloco 21/21.1/exer09.sql
+++ 03 - Desenvolvimento Back-End/Bloco 21/21.1/exer09.sql
@@ -4,7 +4,8 @@ SELECT MAX(SALARY) AS 'Maior salário'
 FROM employees;
 
 -- 2. Escreva uma query que exiba a diferença entre o maior e o menor salário.
-
+SELECT MAX(SALARY) - MIN(SALARY) AS 'Diferença entre o maior e o menor salário'
+FROM employees;
 
 -- 3. Escreva uma query que exiba a média salarial de cada JOB_ID, ordenando pela média salarial em ordem decrescente.
 
