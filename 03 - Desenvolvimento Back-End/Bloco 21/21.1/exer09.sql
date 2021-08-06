9243e7181e2a1091020a66246e4824aef690aa09
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-06 00:01:33 -0300

    21.1 exer09 quest05

--- 03 - Desenvolvimento Back-End/Bloco 21/21.1/exer09.sql
+++ 03 - Desenvolvimento Back-End/Bloco 21/21.1/exer09.sql
@@ -17,7 +17,11 @@ ORDER BY `Média salarial` DESC;
 SELECT SUM(SALARY) AS 'Total de salários' FROM employees;
 
 -- 5. Escreva uma query que exiba quatro informações: o maior salário, o menor salário, a soma de todos os salários e a média dos salários. Todos os valores devem ser formatados para ter apenas duas casas decimais.
-
+SELECT ROUND(MAX(SALARY),2) AS 'Maior salário',
+ROUND(MIN(SALARY),2) AS 'Menor salário',
+ROUND(SUM(SALARY),2) AS 'Soma de todos os salários',
+ROUND(AVG(SALARY),2) AS 'Média dos salários'
+FROM employees;
 
 -- 6. Escreva uma query que exiba a quantidade de pessoas que trabalham como pessoas programadoras (IT_PROG).
 
