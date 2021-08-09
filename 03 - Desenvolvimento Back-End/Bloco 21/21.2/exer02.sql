7eb6b42d5c9c030447a7d84931a030e668a966ec
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-08 22:05:37 -0300

    21.2 exer02 quest02

--- 03 - Desenvolvimento Back-End/Bloco 21/21.2/exer02.sql
+++ 03 - Desenvolvimento Back-End/Bloco 21/21.2/exer02.sql
@@ -1,9 +1,30 @@
 USE sakila;
 -- 1. Queremos saber os ids e custos de substituição dos filmes que possuem o mesmo custo de substituição.
-SELECT t1.film_id, t1.replacement_cost, t2.film_id, t2.replacement_cost
-FROM film AS t1,
-film AS t2
-WHERE t1.replacement_cost = t2.replacement_cost
-AND t1.film_id <> t2.film_id;
+SELECT
+	t1.film_id,
+	t1.replacement_cost,
+    t2.film_id,
+    t2.replacement_cost
+FROM
+	film AS t1,
+	film AS t2
+WHERE
+	t1.replacement_cost = t2.replacement_cost
+AND
+	t1.film_id <> t2.film_id;
 
 -- 2. Exiba o título e a duração de empréstimo dos filmes que possuem a mesma duração. Exiba apenas os filmes com a duração de empréstimo entre 2 e 4 dias.
+SELECT
+	t1.title,
+    t1.rental_duration,
+    t2.title,
+    t2.rental_duration
+FROM
+	film AS t1,
+	film AS t2
+WHERE
+	t1.rental_duration = t2.rental_duration
+AND
+	t1.title <> t2.title
+HAVING
+	t1.rental_duration BETWEEN (2) AND (4);
