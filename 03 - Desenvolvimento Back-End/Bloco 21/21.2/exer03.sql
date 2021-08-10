93e123ea119fbe8c3a37ecd7a64018308d252b6d
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-10 10:02:05 -0300

    21.2 exer03 quest02

--- 03 - Desenvolvimento Back-End/Bloco 21/21.2/exer03.sql
+++ 03 - Desenvolvimento Back-End/Bloco 21/21.2/exer03.sql
@@ -13,5 +13,25 @@ FROM
 	staff);
 
 -- 2. Monte uma query que una os resultados das tabelas customer e actor, exibindo os nomes que contêm a palavra "tracy" na tabela customer e os que contêm "je" na tabela actor. Exiba apenas os resultados únicos.
+(SELECT
+	first_name AS 'Nome',
+    last_name AS 'Sobrenome'
+FROM
+	customer
+WHERE
+	first_name
+LIKE
+	'%tracy%')
+UNION
+(SELECT
+	first_name AS 'Nome',
+    last_name AS 'Sobrenome'
+FROM
+	actor
+WHERE
+	first_name
+LIKE
+	'%je%');
+
 -- 3. Monte uma query que exiba a união dos cinco últimos nomes da tabela actor, o primeiro nome da tabela staff e cinco nomes a partir da 15ª posição da tabela customer. Não permita que dados repetidos sejam exibidos. Ordene os resultados em ordem alfabética.
 -- 4. Você quer exibir uma lista paginada com os nomes e sobrenomes de todos os clientes e atores do banco de dados, em ordem alfabética. Considere que a paginação está sendo feita de 15 em 15 resultados e que você está na 4ª página. Monte uma query que simule esse cenário.
