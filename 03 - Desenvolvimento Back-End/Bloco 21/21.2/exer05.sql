93ee956c7107dbbb26725db6343104f6f5609a79
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-10 14:39:13 -0300

    21.2 exer05 quest09

--- 03 - Desenvolvimento Back-End/Bloco 21/21.2/exer05.sql
+++ 03 - Desenvolvimento Back-End/Bloco 21/21.2/exer05.sql
@@ -134,6 +134,20 @@ WHERE EXISTS (
 );
 
 -- Exercício 9: Utilizando o EXISTS, selecione o nome e localização dos cinemas que não possuem filmes em cartaz.
+SELECT
+	name AS 'Nome',
+	location AS 'Localização'
+FROM
+	Theater AS T
+WHERE NOT EXISTS(
+	SELECT
+		*
+	FROM
+		Movies AS M
+	WHERE
+		T.id = M.theater_id
+);
+
 -- Exercício 10: Utilizando o INNER JOIN, selecione todas as informações dos filmes com avaliação maior que 8 e que estejam em cartaz.
 -- Exercício 11: Utilizando o SELF JOIN, selecione os títulos e duração dos filmes que possuem o mesmo diretor.
 -- Exercício 12: Faça duas buscas, uma utilizando SUBQUERY e outra utilizando INNER JOIN, que retornem o título dos filmes que arrecadaram 500 milhões ou mais, e que possuem duração maior que 110 minutos.
