ad1140768efd1512428a72b58fdada1e4e2a4776
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-10 12:40:42 -0300

    21.2 exer05 quest02

--- 03 - Desenvolvimento Back-End/Bloco 21/21.2/exer05.sql
+++ 03 - Desenvolvimento Back-End/Bloco 21/21.2/exer05.sql
@@ -12,6 +12,18 @@ ON
 	M.id = BO.movie_id;
 
 -- Exercício 2: Utilizando o INNER JOIN, faça uma busca que retorne o número de vendas para cada filme que possui um número maior de vendas internacionais (international_sales) do que vendas nacionais (domestic_sales).
+SELECT
+	M.title AS 'Título',
+    BO.international_sales + BO.domestic_sales AS 'Total de vendas'
+FROM
+	Movies AS M
+INNER JOIN
+	BoxOffice AS BO
+ON
+	M.id = BO.movie_id
+WHERE
+	BO.international_sales > BO.domestic_sales;
+
 -- Exercício 3: Utilizando o INNER JOIN, faça uma busca que retorne os filmes e sua avaliação (rating) em ordem decrescente.
 -- Exercício 4: Utilizando o LEFT JOIN, faça uma busca que retorne todos os dados dos cinemas, mesmo os que não possuem filmes em cartaz e, adicionalmente, os dados dos filmes que estão em cartaz nestes cinemas. Retorne os nomes dos cinemas em ordem alfabética.
 -- Exercício 5: Utilizando o RIGHT JOIN, faça uma busca que retorne todos os dados dos filmes, mesmo os que não estão em cartaz e, adicionalmente, os dados dos cinemas que possuem estes filmes em cartaz. Retorne os nomes dos cinemas em ordem alfabética.
