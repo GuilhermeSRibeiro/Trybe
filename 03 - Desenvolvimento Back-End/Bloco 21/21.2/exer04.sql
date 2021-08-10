8d2f11db135562e1392bfac865f7d94aedf66e85
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-10 11:33:33 -0300

    21.2 exer04 quest01

--- 03 - Desenvolvimento Back-End/Bloco 21/21.2/exer04.sql
+++ 03 - Desenvolvimento Back-End/Bloco 21/21.2/exer04.sql
@@ -1,5 +1,19 @@
 USE hotel;
 -- 1. Usando o EXISTS na tabela books_lent e books, exiba o id e título dos livros que ainda não foram emprestados.
+SELECT
+	Id,
+    Title AS 'Título'
+FROM
+	Books
+WHERE NOT EXISTS (
+SELECT
+	*
+FROM
+	Books_Lent
+WHERE
+	Books.Id = Books_Lent.book_id
+);
+
 -- 2. Usando o EXISTS na tabela books_lent e books, exiba o id e título dos livros estão atualmente emprestados e que contêm a palavra "lost" no título.
 -- 3. Usando a tabela carsales e customers, exiba apenas o nome dos clientes que ainda não compraram um carro.
 -- 4. Usando o comando EXISTS em conjunto com JOIN e as tabelas cars, customers e carsales, exiba o nome do cliente e o modelo do carro de todos os clientes que fizeram compras de carros.
