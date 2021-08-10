4714885100d427fa6295a35d06e95e903ed338f6
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-10 11:56:55 -0300

    21.2 exer04 quest03 correçao

--- 03 - Desenvolvimento Back-End/Bloco 21/21.2/exer04.sql
+++ 03 - Desenvolvimento Back-End/Bloco 21/21.2/exer04.sql
@@ -36,7 +36,7 @@ SELECT
 	Name AS 'Nome'
 FROM
 	Customers AS C
-WHERE EXISTS (
+WHERE NOT EXISTS (
 SELECT
 	*
 FROM
@@ -46,3 +46,4 @@ WHERE
 );
 
 -- 4. Usando o comando EXISTS em conjunto com JOIN e as tabelas cars, customers e carsales, exiba o nome do cliente e o modelo do carro de todos os clientes que fizeram compras de carros.
+
