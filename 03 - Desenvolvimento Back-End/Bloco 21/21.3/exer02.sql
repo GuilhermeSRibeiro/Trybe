160f30f1ed8c95813e78a1f24dcfb24bbdc70987
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-13 14:12:47 -0300

    21.3 exer02 quest01

--- 03 - Desenvolvimento Back-End/Bloco 21/21.3/exer02.sql
+++ 03 - Desenvolvimento Back-End/Bloco 21/21.3/exer02.sql
@@ -1,5 +1,19 @@
 USE sakila;
 
 -- 1. Utilizando a tabela sakila.payment, monte uma function que retorna a quantidade total de pagamentos feitos até o momento por um determinado customer_id.
+DELIMITER $$
+CREATE FUNCTION RetornaTotalDePagamentoDeUmCliente(id INT)
+RETURNS DECIMAL READS SQL DATA
+BEGIN
+	DECLARE total DECIMAL;
+    SELECT COUNT(*)
+    FROM payment
+    WHERE customer_id = id
+    INTO total;
+    RETURN total;
+END $$
+DELIMITER ;
+SELECT RetornaTotalDePagamentoDeUmCliente(1);
+
 -- 2. Crie uma function que, dado o parâmetro de entrada inventory_id, retorna o nome do filme vinculado ao registro de inventário com esse id.
 -- 3. Crie uma function que receba uma determinada categoria de filme em formato de texto (ex: 'Action', 'Horror') e retorna a quantidade total de filmes registrados nessa categoria.
