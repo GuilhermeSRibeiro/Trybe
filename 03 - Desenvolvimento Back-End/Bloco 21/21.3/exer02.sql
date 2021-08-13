bf6ff1e5288c626566f713f52699b85de420a399
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-13 14:28:19 -0300

    21.3 exer02 quest02

--- 03 - Desenvolvimento Back-End/Bloco 21/21.3/exer02.sql
+++ 03 - Desenvolvimento Back-End/Bloco 21/21.3/exer02.sql
@@ -16,4 +16,19 @@ DELIMITER ;
 SELECT RetornaTotalDePagamentoDeUmCliente(1);
 
 -- 2. Crie uma function que, dado o parâmetro de entrada inventory_id, retorna o nome do filme vinculado ao registro de inventário com esse id.
+DELIMITER $$
+CREATE FUNCTION RetornaFilmePeloIdDoInventario(inventory_id INT)
+RETURNS VARCHAR(100) READS SQL DATA
+BEGIN
+	DECLARE filme VARCHAR(100);
+    SELECT title
+    FROM film AS f
+    INNER JOIN inventory AS i
+    ON f.film_id = i.film_id
+    WHERE i.film_id = inventory_id
+    INTO filme;
+END $$
+DELIMITER ;
+SELECT RetornaFilmePeloIdDoInventario(1);
+
 -- 3. Crie uma function que receba uma determinada categoria de filme em formato de texto (ex: 'Action', 'Horror') e retorna a quantidade total de filmes registrados nessa categoria.
