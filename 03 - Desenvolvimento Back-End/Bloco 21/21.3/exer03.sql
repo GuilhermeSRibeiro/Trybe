7c987d8bb836384ee5f63556167b06c376ac9793
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-13 14:55:35 -0300

    21.3 exer03 quest01

--- 03 - Desenvolvimento Back-End/Bloco 21/21.3/exer03.sql
+++ 03 - Desenvolvimento Back-End/Bloco 21/21.3/exer03.sql
@@ -1,3 +1,15 @@
 -- 1. Crie um TRIGGER que, a cada nova inserção feita na tabela carros, defina o valor da coluna data_atualizacao para o momento do ocorrido, a acao para 'INSERÇÃO' e a coluna disponivel_em_estoque para 1.
+USE betrybe_automoveis;
+DELIMITER $$
+CREATE TRIGGER Primeiro
+BEFORE INSERT ON carros
+FOR EACH ROW
+BEGIN
+	SET NEW.data_atualizacao = NOW(),
+    NEW.acao = 'INSERÇÃO',
+    NEW.disponivel_em_estoque = 1;
+END $$
+DELIMITER ;
+
 -- 2. Crie um TRIGGER que, a cada atualização feita na tabela carros, defina o valor da coluna data_atualizacao para o momento do ocorrido e a acao para 'ATUALIZAÇÃO'.
 -- 3. Crie um TRIGGER que, a cada exclusão feita na tabela carros, envie para a tabela log_operacoes as informações do tipo_operacao como 'EXCLUSÃO' e a data_ocorrido como o momento da operação.
