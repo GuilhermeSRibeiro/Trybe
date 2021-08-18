507485c11ab44622f6e227545ae113ac927e7dde
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-18 20:07:00 -0300

    22.1 exer01 quest02

--- 03 - Desenvolvimento Back-End/Bloco 22/22.1/exer01.sql
+++ 03 - Desenvolvimento Back-End/Bloco 22/22.1/exer01.sql
@@ -5,3 +5,12 @@ CREATE TABLE Estilo_Musical(
     Nome VARCHAR(50) NOT NULL
 ) ENGINE=InnoDB;
 
+CREATE TABLE Album(
+	Album_id INT PRIMARY KEY AUTO_INCREMENT,
+    Artista_id INT NOT NULL,
+    Titulo VARCHAR(100) NOT NULL,
+    Preco DECIMAL(5,2) NOT NULL,
+    Estilo_id INT NOT NULL,
+    FOREIGN KEY (Artista_id) REFERENCES Artista(Artista_id),
+    FOREIGN KEY (Estilo_id) REFERENCES Estilo_Musical(Estilo_id)
+) ENGINE=InnoDB;
