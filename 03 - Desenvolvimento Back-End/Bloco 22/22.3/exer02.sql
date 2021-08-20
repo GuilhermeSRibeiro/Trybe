9c54b3c60013834823fbf12d9d036e6be182f5d7
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-20 11:21:33 -0300

    22.3 exer02.sql

---
+++ 03 - Desenvolvimento Back-End/Bloco 22/22.3/exer02.sql
@@ -0,0 +1,16 @@
+USE sakila;
+
+CREATE VIEW top_10_customers AS
+SELECT c.customer_id,
+c.first_name,
+SUM(p.amount) AS `Total amount`
+FROM customer AS c
+INNER JOIN
+payment AS p
+ON
+c.customer_id = p.customer_id
+GROUP BY
+customer_id
+ORDER BY
+`Total amount` DESC
+LIMIT 10;
