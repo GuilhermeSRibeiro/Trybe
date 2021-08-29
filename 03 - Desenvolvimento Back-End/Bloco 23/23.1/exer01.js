bd123ca7f22da2415a5cea29b4d3fcd1539cfd87
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-29 00:41:32 -0300

    23.1 exer01 quest03

--- 03 - Desenvolvimento Back-End/Bloco 23/23.1/exer01.js
+++ 03 - Desenvolvimento Back-End/Bloco 23/23.1/exer01.js
@@ -9,6 +9,9 @@ use("sample");
 db.bios.find({_id: 8}, {"_id": 1, "name": 1}).pretty();
 
 // Exercício 3: Retorne apenas os atributos name e birth do documento com o _id igual a 8.
+use("sample");
+db.bios.find({_id: 8}, {"name": 1, "birth": 1, "_id": 0}).pretty();
+
 // Exercício 4: Retorne todos os documentos em que o atributo name.first seja igual a John, utilizando o método pretty().
 // Exercício 5: Retorne os 3 primeiros documentos da coleção bios utilizando o método pretty().
 // Exercício 6: Retorne 2 documentos da coleção bios pulando os 5 primeiros documentos.
