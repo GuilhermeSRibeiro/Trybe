b2b031bdb7176cd7a4fd15d720e1fb7b6eb6cff2
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-29 00:43:47 -0300

    23.1 exer01 quest04

--- 03 - Desenvolvimento Back-End/Bloco 23/23.1/exer01.js
+++ 03 - Desenvolvimento Back-End/Bloco 23/23.1/exer01.js
@@ -13,6 +13,9 @@ use("sample");
 db.bios.find({_id: 8}, {"name": 1, "birth": 1, "_id": 0}).pretty();
 
 // Exercício 4: Retorne todos os documentos em que o atributo name.first seja igual a John, utilizando o método pretty().
+use("sample");
+db.bios.find({"name.first": "John"}).pretty();
+
 // Exercício 5: Retorne os 3 primeiros documentos da coleção bios utilizando o método pretty().
 // Exercício 6: Retorne 2 documentos da coleção bios pulando os 5 primeiros documentos.
 // Utilizando o mongoimport, importe o arquivo books.json para a sua instância local do MongoDB e utilize a coleção books para construir as seguintes consultas:
