a7fa3db4e1d27803b552ef7292f55c9dba6626c8
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-29 04:16:56 -0300

    23.2 exer01 quest02

--- 03 - Desenvolvimento Back-End/Bloco 23/23.2/exer01.js
+++ 03 - Desenvolvimento Back-End/Bloco 23/23.2/exer01.js
@@ -3,6 +3,9 @@ use("business");
 db.restaurants.find({ borough: { $in: ["Queens", "Staten Island", "Bronx"] } }).count();
 
 // 2. Selecione e faça a contagem dos restaurantes que não possuem culinária do tipo American (utilizando o atributo cuisine).
+use("business");
+db.restaurants.find({ cuisine: { $ne: "American" } }).count();
+
 // 3. Selecione e faça a contagem dos restaurantes que possuem avaliação maior ou igual a 8 (utilizando o atributo rating).
 // 4. Selecione e faça a contagem dos restaurantes que possuem avaliação menor que 4.
 // 5. Selecione e faça a contagem dos restaurantes que não possuem as avaliações 5, 6 e 7.
