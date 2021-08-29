d93c77918f1b4db78b63dc39578255cfadabbf51
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-29 04:17:58 -0300

    23.2 exer01 quest03

--- 03 - Desenvolvimento Back-End/Bloco 23/23.2/exer01.js
+++ 03 - Desenvolvimento Back-End/Bloco 23/23.2/exer01.js
@@ -7,5 +7,8 @@ use("business");
 db.restaurants.find({ cuisine: { $ne: "American" } }).count();
 
 // 3. Selecione e faça a contagem dos restaurantes que possuem avaliação maior ou igual a 8 (utilizando o atributo rating).
+use("business");
+db.restaurants.find({ rating: { $gte: 8 } }).count();
+
 // 4. Selecione e faça a contagem dos restaurantes que possuem avaliação menor que 4.
 // 5. Selecione e faça a contagem dos restaurantes que não possuem as avaliações 5, 6 e 7.
