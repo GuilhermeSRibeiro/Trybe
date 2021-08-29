834b79cdc597b2694515343f1fba5446d87d4559
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-29 04:15:20 -0300

    23.2 exer01 quest01

--- 03 - Desenvolvimento Back-End/Bloco 23/23.2/exer01.js
+++ 03 - Desenvolvimento Back-End/Bloco 23/23.2/exer01.js
@@ -1,4 +1,7 @@
 // 1. Selecione e faça a contagem dos restaurantes presentes nos bairros Queens, Staten Island e Bronx (utilizando o atributo borough).
+use("business");
+db.restaurants.find({ borough: { $in: ["Queens", "Staten Island", "Bronx"] } }).count();
+
 // 2. Selecione e faça a contagem dos restaurantes que não possuem culinária do tipo American (utilizando o atributo cuisine).
 // 3. Selecione e faça a contagem dos restaurantes que possuem avaliação maior ou igual a 8 (utilizando o atributo rating).
 // 4. Selecione e faça a contagem dos restaurantes que possuem avaliação menor que 4.
