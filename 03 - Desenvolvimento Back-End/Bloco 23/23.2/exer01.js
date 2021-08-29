379005d8b131f5affb1b8fef77299642df226ca9
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-29 04:18:53 -0300

    23.2 exer01 quest04

--- 03 - Desenvolvimento Back-End/Bloco 23/23.2/exer01.js
+++ 03 - Desenvolvimento Back-End/Bloco 23/23.2/exer01.js
@@ -11,4 +11,7 @@ use("business");
 db.restaurants.find({ rating: { $gte: 8 } }).count();
 
 // 4. Selecione e faça a contagem dos restaurantes que possuem avaliação menor que 4.
+use("business");
+db.restaurants.find({ rating: { $lt: 4 } }).count();
+
 // 5. Selecione e faça a contagem dos restaurantes que não possuem as avaliações 5, 6 e 7.
