c43c3ec53fe4d3bbf0e0b3005a8e50c32ae230af
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-29 15:15:17 -0300

    23.2 exer03 quest01

--- 03 - Desenvolvimento Back-End/Bloco 23/23.2/exer03.js
+++ 03 - Desenvolvimento Back-End/Bloco 23/23.2/exer03.js
@@ -1,4 +1,6 @@
 // 1. Ordene alfabeticamente os restaurantes pelo nome (atributo name).
+use("business");
+db.restaurants.find().sort({ name: 1});
 
 
 // 2. Ordene os restaurantes de forma descrescente baseado nas avaliações.
