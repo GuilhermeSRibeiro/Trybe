ab67c9ad0bf35f722f90576387ce258dc67d52c1
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-30 22:46:48 -0300

    24.2 exer01 quest11b

--- 03 - Desenvolvimento Back-End/Bloco 24/24.2/exer01.js
+++ 03 - Desenvolvimento Back-End/Bloco 24/24.2/exer01.js
@@ -173,7 +173,19 @@ db.movies.updateOne(
 );
 db.movies.find();
 
-// * Adicione o campo actor, que deve ser um array com o valor Michael Caine, ao array de cast em que o campo character seja igual a Alfred ;
+// * Adicione o campo actor, que deve ser um array com o valor Michael Caine, ao array de cast em que o campo character seja igual a Alfred;
+use("class");
+db.movies.updateOne(
+  {
+    title: "Batman",
+    "cast.character": "Alfred",
+  },
+  {
+    $push: { "cast.$.actor": "Michael Caine" },
+  },
+);
+db.movies.find();
+
 // * Adicione o campo actor, que deve ser um array com o valor Heath Ledger, ao array de cast em que o campo character seja igual a Coringa.
 // Dica: Para isso, leia aqui sobre o operador $.
 
