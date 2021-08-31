b0b09a54af9ecc46401169be42f956d38036fc74
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-30 22:46:00 -0300

    24.2 exer01 quest11a

--- 03 - Desenvolvimento Back-End/Bloco 24/24.2/exer01.js
+++ 03 - Desenvolvimento Back-End/Bloco 24/24.2/exer01.js
@@ -160,7 +160,19 @@ db.movies.updateOne(
 db.movies.find();
 
 // Exercício 11: Produza três querys para o filme Batman :
-// * Adicione o campo actor, que deve ser um array com o valor Christian Bale, ao array de cast em que o campo character seja igual a Batman ;
+// * Adicione o campo actor, que deve ser um array com o valor Christian Bale, ao array de cast em que o campo character seja igual a Batman;
+use("class");
+db.movies.updateOne(
+  {
+    title: "Batman",
+    "cast.character": "Batman",
+  },
+  {
+    $push: { "cast.$.actor": "Christian Bale" },
+  },
+);
+db.movies.find();
+
 // * Adicione o campo actor, que deve ser um array com o valor Michael Caine, ao array de cast em que o campo character seja igual a Alfred ;
 // * Adicione o campo actor, que deve ser um array com o valor Heath Ledger, ao array de cast em que o campo character seja igual a Coringa.
 // Dica: Para isso, leia aqui sobre o operador $.
