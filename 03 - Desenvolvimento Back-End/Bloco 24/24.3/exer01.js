f5069c636ace8f9af5a578f0c50403c9826acc68
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-31 16:57:01 -0300

    24.3 exer01 quest06

--- 03 - Desenvolvimento Back-End/Bloco 24/24.3/exer01.js
+++ 03 - Desenvolvimento Back-End/Bloco 24/24.3/exer01.js
@@ -63,7 +63,21 @@ db.movies.updateOne(
 db.movies.find();
 
 // 06. Retorne todos os filmes com ratings maior do que 103, exibindo apenas os campos title e ratings.
-
+use("class");
+db.movies.find(
+  {
+    ratings: {
+      $elemMatch: {
+        $gt: 103,
+      },
+    },
+  },
+  {
+    _id: 0,
+    title: 1,
+    ratings: 1,
+  },
+).pretty();
 
 // 07. Retorne todos os filmes com ratings entre 100 e 105, exibindo apenas os campos title e ratings.
 
