f21d244e1560a40021d6227f67be405a33116708
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-30 22:51:57 -0300

    24.2 exer01 quest12

--- 03 - Desenvolvimento Back-End/Bloco 24/24.2/exer01.js
+++ 03 - Desenvolvimento Back-End/Bloco 24/24.2/exer01.js
@@ -202,4 +202,19 @@ db.movies.find();
 
 // Exercício 12: Adicione aos atores de cast do character Batman do filme Batman os valores "Michael Keaton", "Val Kilmer" e "George Clooney", e deixe o array em ordem alfabética.
 // Dica: Para isso, leia aqui sobre o operador $.
-
+use("class");
+db.movies.updateOne(
+  {
+    title: "Batman",
+    "cast.character": "Batman",
+  },
+  {
+    $push: {
+      "cast.$.actor": {
+        $each: ["Michael Keaton", "Val Kilmer", "George Clooney"],
+        $sort: 1,
+      },
+    },
+  },
+);
+db.movies.find();
