0e1d72231a2413f0374e73552bb275df44bb90c8
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-01 18:42:52 -0300

    25.1 exer01 quest02

--- 03 - Desenvolvimento Back-End/Bloco 25/25.1/exer01.js
+++ 03 - Desenvolvimento Back-End/Bloco 25/25.1/exer01.js
@@ -7,7 +7,17 @@ db.transactions.aggregate(
 );
 
 // 2. Selecione todas as transações com o valor entre 700 e 6000, ou que sejam recebidas pela cliente "Lisa Simpson".
-
+use("agg_example");
+db.transactions.aggregate(
+  [{
+    $match: {
+      $or: [
+        { value: { $gte: 700, $lte: 6000 } },
+        { to: "Lisa Simpson" },
+      ],
+    },
+  }],
+);
 
 // 3. Selecione três transações com o valor acima de 1000.
 
