75b82364771168623102943965cda5ca11fcea0e
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-01 18:46:27 -0300

    25.1 exer01 quest03

--- 03 - Desenvolvimento Back-End/Bloco 25/25.1/exer01.js
+++ 03 - Desenvolvimento Back-End/Bloco 25/25.1/exer01.js
@@ -20,4 +20,14 @@ db.transactions.aggregate(
 );
 
 // 3. Selecione três transações com o valor acima de 1000.
-
+use("agg_example");
+db.transactions.aggregate(
+  [
+    {
+      $match: { value: { $gt: 1000 } },
+    },
+    {
+      $limit: 3,
+    },
+  ],
+);
