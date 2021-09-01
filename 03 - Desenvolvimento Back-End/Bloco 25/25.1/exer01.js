d58178eadc40905250f18ad5c7bc8b3e275d13ca
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-01 18:36:36 -0300

    25.1 exer01 enunciados

---
+++ 03 - Desenvolvimento Back-End/Bloco 25/25.1/exer01.js
@@ -0,0 +1,8 @@
+// 1. Selecione todas as transações feitas pelo cliente chamado "Dave America".
+
+
+// 2. Selecione todas as transações com o valor entre 700 e 6000, ou que sejam recebidas pela cliente "Lisa Simpson".
+
+
+// 3. Selecione três transações com o valor acima de 1000.
+
