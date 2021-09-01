84c83f04e6177694468c1c1fbf84bb90745c4c2d
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-01 18:39:53 -0300

    25.1 exer01 quest01

--- 03 - Desenvolvimento Back-End/Bloco 25/25.1/exer01.js
+++ 03 - Desenvolvimento Back-End/Bloco 25/25.1/exer01.js
@@ -1,5 +1,10 @@
 // 1. Selecione todas as transações feitas pelo cliente chamado "Dave America".
-
+use("agg_example");
+db.transactions.aggregate(
+  [{
+    $match: { from: "Dave America" },
+  }],
+);
 
 // 2. Selecione todas as transações com o valor entre 700 e 6000, ou que sejam recebidas pela cliente "Lisa Simpson".
 
