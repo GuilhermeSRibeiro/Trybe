065806e502a65984d6b0ee52ae94a596851c746b
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-02 21:51:56 -0300

    25.1 cria exer02.js

---
+++ 03 - Desenvolvimento Back-End/Bloco 25/25.1/exer02.js
@@ -0,0 +1,11 @@
+// 1. Selecione todos os bancos, ou seja, valores do campo bank;
+
+
+// 2. Selecione o valor total das transações em cada banco e quantas são;
+
+
+// 3. Selecione o valor total de transações por banco;
+
+
+// 4. Selecione os bancos que têm o valor total de transações maior que 1000.
+
