20e90aa0791e003303f2b53f8299ef206ef05169
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-03 23:02:36 -0300

    25.1 exer04 quest01

--- 03 - Desenvolvimento Back-End/Bloco 25/25.1/exer04.js
+++ 03 - Desenvolvimento Back-End/Bloco 25/25.1/exer04.js
@@ -1,5 +1,10 @@
 // Exercício 1: Utilizando o estágio $match, escreva uma agregação para retornar somente os clientes do sexo "MASCULINO".
-
+use("erp");
+db.clientes.aggregate([
+  {
+    $match: { sexo: "MASCULINO" },
+  },
+]);
 
 // Exercício 2: Utilizando o estágio $match, escreva uma agregação para retornar somente os clientes do sexo "FEMININO" e com data de nascimento entre os anos de 1995 e 2005.
 
