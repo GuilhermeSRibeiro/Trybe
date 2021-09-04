7eac40ebe146834cadd2b40bacb10cc264cad9ca
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-03 21:37:20 -0300

    25.1 exer03 quest01

--- 03 - Desenvolvimento Back-End/Bloco 25/25.1/exer03.js
+++ 03 - Desenvolvimento Back-End/Bloco 25/25.1/exer03.js
@@ -1,5 +1,16 @@
 // 1. Selecione todos os clientes com as suas respectivas transações feitas;
-
+use("agg_example");
+db.transactions.find();
+db.clients.aggregate([
+  {
+    $lookup: {
+      from: "transactions",
+      localField: "name",
+      foreignField: "from",
+      as: "teste",
+    },
+  },
+]);
 
 // 2. Selecione quatro clientes com as suas respectivas transações recebidas;
 
