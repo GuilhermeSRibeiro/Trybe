5db1e0cbc1e66e20cc8ed4246c59930790133cae
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-03 21:36:55 -0300

    25.1 cria exer03.js

---
+++ 03 - Desenvolvimento Back-End/Bloco 25/25.1/exer03.js
@@ -0,0 +1,8 @@
+// 1. Selecione todos os clientes com as suas respectivas transações feitas;
+
+
+// 2. Selecione quatro clientes com as suas respectivas transações recebidas;
+
+
+// 3. Selecione todos os cliente do estado da "Florida" e suas respectivas transações recebidas.
+
