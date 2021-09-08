54e65f17180462e69d94ac08ace4ddbeb6cf8034
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-08 20:58:37 -0300

    25.2 cria exer06.js

---
+++ 03 - Desenvolvimento Back-End/Bloco 25/25.2/exer06.js
@@ -0,0 +1,5 @@
+// 1. Calcule qual o valor total em estoque de cada produto, considerando o preço de venda e a quantidade;
+
+
+// 2. Calcule qual será o lucro total de cada produto caso todo o estoque seja vendido.
+
