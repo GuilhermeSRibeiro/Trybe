e4dc45f39639953d1f8aad9b1e8217b13b3a7eb4
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-08 20:48:24 -0300

    25.2 exer04 quest01

--- 03 - Desenvolvimento Back-End/Bloco 25/25.2/exer04.js
+++ 03 - Desenvolvimento Back-End/Bloco 25/25.2/exer04.js
@@ -1,5 +1,14 @@
 // 1. Retorne o menor número inteiro relativo ao preço de venda de cada produto;
-
+db.products.aggregate([
+  {
+    $project: {
+      nome: '$name',
+      'menor valor relativo': {
+        $floor: '$sale_price',
+      },
+    },
+  },
+]);
 
 // 2. Retorne o maior número inteiro relativo ao lucro total sobre cada produto. Nota: Desconsiderar taxas (taxes).
 
