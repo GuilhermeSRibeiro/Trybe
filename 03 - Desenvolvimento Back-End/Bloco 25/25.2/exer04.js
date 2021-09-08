60f706001086a0b5fc85ffcbfb259ce29449b790
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-08 20:44:10 -0300

    25.2 cria exer04.js

---
+++ 03 - Desenvolvimento Back-End/Bloco 25/25.2/exer04.js
@@ -0,0 +1,5 @@
+// 1. Retorne o menor número inteiro relativo ao preço de venda de cada produto;
+
+
+// 2. Retorne o maior número inteiro relativo ao lucro total sobre cada produto. Nota: Desconsiderar taxas (taxes).
+
