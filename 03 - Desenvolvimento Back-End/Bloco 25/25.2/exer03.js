c6f13f5eb8abf9d4d7d9ee95c694a8b5ec181729
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-08 20:42:46 -0300

    25.2 exer03 quest01

--- 03 - Desenvolvimento Back-End/Bloco 25/25.2/exer03.js
+++ 03 - Desenvolvimento Back-End/Bloco 25/25.2/exer03.js
@@ -1,1 +1,16 @@
 // Calcule qual o lucro total de cada produto, considerando o preço de compra, os impostos e seu valor de venda.
+db.products.aggregate([
+  {
+    $project: {
+      nome: '$name',
+      'lucro total': {
+        $subtract: [
+          '$sale_price',
+          {
+            $add: ['$purchase_price', '$taxes'],
+          },
+        ],
+      },
+    },
+  },
+]);
