39a6b6e796c2d695ac90d54689b228e2c50a83ac
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-08 20:14:01 -0300

    25.2 exer01 quest02

--- 03 - Desenvolvimento Back-End/Bloco 25/25.2/exer01.js
+++ 03 - Desenvolvimento Back-End/Bloco 25/25.2/exer01.js
@@ -19,7 +19,28 @@ db.clients.aggregate([
 ]);
 
 // 2. Selecione os quatro primeiros clientes com as suas respectivas transações recebidas ordenados pelo estado em ordem alfabética;
-
+db.clients.aggregate([
+  {
+    $lookup: {
+      from: 'transactions',
+      let: { nome: '$name' },
+      pipeline: [
+        {
+          $match: {
+            $expr: {
+              $eq: ['$to', '$$nome'],
+            },
+          },
+        },
+      ],
+      as: 'transações_recebidas',
+    },
+  },
+  {
+    $sort: { State: 1 },
+  },
+  { $limit: 4 },
+]);
 
 // 3. Selecione todos os cliente do estado da "Florida" e suas respectivas transações recebidas.
 
