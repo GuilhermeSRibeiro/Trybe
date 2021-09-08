7acfce6232bfaa0bf22bb41ad377e22637b4728e
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-08 20:36:30 -0300

    25.2 exer02 quest01

--- 03 - Desenvolvimento Back-End/Bloco 25/25.2/exer02.js
+++ 03 - Desenvolvimento Back-End/Bloco 25/25.2/exer02.js
@@ -1,1 +1,11 @@
 // Calcule qual o custo total de cada produto, considerando o preço de compra e os impostos.
+db.products.aggregate([
+  {
+    $project: {
+      nome: '$name',
+      'custo_total': {
+        $add: ['$purchase_price', '$taxes'],
+      },
+    },
+  },
+]);
