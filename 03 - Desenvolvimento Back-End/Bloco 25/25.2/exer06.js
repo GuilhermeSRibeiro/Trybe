6106d8d553f098001b7de20ecd303cc91831dfac
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-08 21:01:29 -0300

    25.2 exer06 quest01

--- 03 - Desenvolvimento Back-End/Bloco 25/25.2/exer06.js
+++ 03 - Desenvolvimento Back-End/Bloco 25/25.2/exer06.js
@@ -1,5 +1,14 @@
 // 1. Calcule qual o valor total em estoque de cada produto, considerando o preço de venda e a quantidade;
-
+db.products.aggregate([
+  {
+    $project: {
+      nome: '$name',
+      'total em estoque': {
+        $multiply: ['$sale_price', '$quantity'],
+      },
+    },
+  },
+]);
 
 // 2. Calcule qual será o lucro total de cada produto caso todo o estoque seja vendido.
 
