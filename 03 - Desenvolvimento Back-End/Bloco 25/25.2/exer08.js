93ad166e543ec827838a14cf6d352d82c60e1c75
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-08 21:29:09 -0300

    25.2 exer08 quest01

--- 03 - Desenvolvimento Back-End/Bloco 25/25.2/exer08.js
+++ 03 - Desenvolvimento Back-End/Bloco 25/25.2/exer08.js
@@ -1,1 +1,10 @@
 // Calcule o valor total do estoque, considerando que cada produto valha o mesmo que seu preço de venda. Lembre-se da quantidade.
+db.products.aggregate([
+  {
+    $addFields: {
+      'total de estoque': {
+        $multiply: ['$sale_price', '$quantity'],
+      },
+    },
+  },
+]);
