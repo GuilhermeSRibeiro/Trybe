7cd8b59b3f2da3aee1169548b043aab260346043
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-08 21:05:11 -0300

    25.2 exer06 quest02

--- 03 - Desenvolvimento Back-End/Bloco 25/25.2/exer06.js
+++ 03 - Desenvolvimento Back-End/Bloco 25/25.2/exer06.js
@@ -11,4 +11,23 @@ db.products.aggregate([
 ]);
 
 // 2. Calcule qual será o lucro total de cada produto caso todo o estoque seja vendido.
-
+db.products.aggregate([
+  {
+    $project: {
+      nome: '$name',
+      'lucro total de venda': {
+        $multiply: [
+          {
+            $subtract: [
+              '$sale_price',
+              {
+                $add: ['$purchase_price', '$taxes'],
+              },
+            ],
+          },
+          '$quantity',
+        ],
+      },
+    },
+  },
+]);
