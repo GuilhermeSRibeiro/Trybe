b9700481d961f962c400b42fea3e99cd6e74111f
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-19 18:51:55 -0300

    26.2 exer01 quest04

--- 03 - Desenvolvimento Back-End/Bloco 26/26.2/exer01.js
+++ 03 - Desenvolvimento Back-End/Bloco 26/26.2/exer01.js
@@ -52,10 +52,43 @@ quest03();
     // Crie uma função que leia todos os dados do arquivo e imprima cada personagem no formato id - Nome. Por exemplo: 1 - Homer Simpson.
     // Crie uma função que receba o id de uma personagem como parâmetro e retorne uma Promise que é resolvida com os dados da personagem que possui o id informado. Caso não haja uma personagem com o id informado, rejeite a Promise com o motivo "id não encontrado".
     // Crie uma função que altere o arquivo simpsons.json retirando os personagens com id 10 e 6.
+
+
     // Crie uma função que leia o arquivo simpsons.json e crie um novo arquivo, chamado simpsonFamily.json, contendo as personagens com id de 1 a 4.
     // Crie uma função que adicione ao arquivo simpsonFamily.json o personagem Nelson Muntz.
     // Crie uma função que substitua o personagem Nelson Muntz pela personagem Maggie Simpson no arquivo simpsonFamily.json.
+const fs = require('fs').promises;
 
+fs.readFile('simpsons.json', 'utf8')
+  .then((data) => {
+    return JSON.parse(data);
+  })
+  .then((array) => {
+    return array.forEach(({ id, name }) => console.log(`${id} - ${name}`));
+  });
+
+async function retornaPersonagem(id) {
+  const simpsons = await fs.readFile('simpsons.json', 'utf8')
+    .then((data) => {
+      return JSON.parse(data);
+    });
+  const busca = simpsons.find((simpson) => simpson.id === id );
+  if (!busca) {
+    throw new Error('id não encontrado');
+  }
+  return busca;
+};
+retornaPersonagem('11');
+
+async function alteraPersonagem() {
+  const simpsons = await fs.readFile('./simpsons.json', 'utf8')
+    .then((data) => {
+      return JSON.parse(data);
+    });
+  const alterado = simpsons.map((simpson) => simpson.id !== '10' || simpson.id !== '6');
+  await fs.writeFile('./simpsons.json', JSON.stringify(alterado));
+};
+alteraPersonagem();
 
 // 5. Crie uma função que lê e escreve vários arquivos ao mesmo tempo.
   // Utilize o Promise.all para manipular vários arquivos ao mesmo tempo.
