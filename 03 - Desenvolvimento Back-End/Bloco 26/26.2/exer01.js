a501b1c3fc53c29a7adfab2e62f1b8bd78f53ecd
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-19 02:42:52 -0300

    26.2 exer01 quest01

--- 03 - Desenvolvimento Back-End/Bloco 26/26.2/exer01.js
+++ 03 - Desenvolvimento Back-End/Bloco 26/26.2/exer01.js
@@ -3,7 +3,22 @@
   // Caso todos os parâmetros sejam numéricos, some os dois primeiros e multiplique o resultado pelo terceiro (a + b) * c.
   // Caso o resultado seja menor que 50, rejeite a Promise com o motivo "Valor muito baixo".
   // Caso o resultado seja maior que 50, resolva a Promise com o valor obtido.
+const quest01 = (a, b, c) => {
+  return new Promise((resolve, reject) => {
+    if (typeof(a) !== 'number' || typeof(b) !== 'number' || typeof(c) !== 'number') {
+      reject('Informe apenas números');
+    }
+    const resultado = (a + b) * c;
+    if (resultado < 50) {
+      reject('Valor muito baixo');
+    }
+    resolve(resultado);
+  });
+};
 
+quest01(1, 2, 3)
+  .then(resolve => console.log(resolve))
+  .catch(reject => console.log(reject));
 
 // 2. Escreva um código para consumir a função construída no exercício anterior.
   // Gere um número aleatório de 1 a 100 para cada parâmetro que a função recebe. Para gerar um número aleatório, utilize o seguinte trecho de código: Math.floor(Math.random() * 100 + 1).
