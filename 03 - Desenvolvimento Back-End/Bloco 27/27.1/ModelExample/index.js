a3d8eab3d30ca8459eaa7890251eccdc51f97826
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-10-05 17:42:12 -0300

    Adiciona rota get /authors

--- 03 - Desenvolvimento Back-End/Bloco 27/27.1/ModelExample/index.js
+++ 03 - Desenvolvimento Back-End/Bloco 27/27.1/ModelExample/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const author = require('./models/author');
 
 const app = express();
 
+const OK_STATUS = 200;
 const LOCAL_PORT = 3000;
 const PORT = process.env.PORT || LOCAL_PORT;
+
+app.get('/authors', async (_req, res) => {
+  const authors = author.getAll();
+  return res.status(OK_STATUS).json(authors);
+});
 
 app.listen(PORT, () => {
   console.log(`Aplicação ouvindo na porta ${PORT}`)
