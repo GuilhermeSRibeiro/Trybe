6f383e9e9accd4a131aeef8b2fba2a7804baf088
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-10-10 23:33:22 -0300

    Adiciona await que estava faltando

--- 03 - Desenvolvimento Back-End/Bloco 27/27.1/model_example/models/books.js
+++ 03 - Desenvolvimento Back-End/Bloco 27/27.1/model_example/models/books.js
@@ -2,7 +2,7 @@ const connection = require('./connection');
 
 const getAll = async () => {
   const query = 'SELECT * FROM ModelExample.books;';
-  const [books] = connection.execute(query);
+  const [books] = await connection.execute(query);
   return books;
 };
 
