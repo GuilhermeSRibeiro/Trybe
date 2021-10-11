ac59cd502f53c80504158b2935307a6d737eaa8d
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-10-10 23:19:46 -0300

    Refatora authors.js

--- 03 - Desenvolvimento Back-End/Bloco 27/27.1/model_example/models/authors.js
+++ 03 - Desenvolvimento Back-End/Bloco 27/27.1/model_example/models/authors.js
@@ -1,9 +1,28 @@
 const connection = require('./connection');
 
+const serialize = (authorData) => ({
+  id: authorData.id,
+  firstName: authorData.first_name,
+  middleName: authorData.middle_name,
+  lastName: authorData.last_name,
+});
+
+const getNewAuthor = (authorData) => {
+  const { id, firstName, middleName, lastName } = authorData;
+  const fullName = [firstName, middleName, lastName].filter((name) => name).join(' ');
+  return {
+    id,
+    firstName,
+    middleName,
+    lastName,
+    name: fullName,
+  };
+};
+
 const getAll = async () => {
   const query = 'SELECT id, first_name, middle_name, last_name FROM ModelExample.authors;';
-  const authors = await connection.execute(query);
-  return authors;
+  const [authors] = await connection.execute(query);
+  return authors.map(serialize).map(getNewAuthor);
 };
 
 module.exports = {
